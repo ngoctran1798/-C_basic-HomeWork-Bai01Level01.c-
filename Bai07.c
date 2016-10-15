89ce596e6eec29450e683a8199fbc60e746f8838
Trần Thị Ngọc <ngoctran1798@gmail.com>
2016-10-15 16:54:46 +0700

    Add files via upload

---
+++ Bai07.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{int ngay,thang,nam;
+    printf("Nhap ngay, thang, nam: ");
+    scanf("%d%d%d",&ngay,&thang,&nam);
+    printf(" ngay %d thang %d nam %d",ngay,thang,nam);
+}
