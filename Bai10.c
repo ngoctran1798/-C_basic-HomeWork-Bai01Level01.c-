938d0486f9920bf7fccfaa763a517751e299e41b
Trần Thị Ngọc <ngoctran1798@gmail.com>
2016-10-15 17:03:43 +0700

    Add files via upload

---
+++ Bai10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{float soThuc;
+    printf("Nhap so thuc: ");
+    scanf("%f",&soThuc);
+    soThuc=(int)soThuc;
+    printf("So nguyen la: %0.2f",soThuc);
+}
+
