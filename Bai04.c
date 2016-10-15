24f0977825fea222cabde6db30f49261a5310883
Trần Thị Ngọc <ngoctran1798@gmail.com>
2016-10-15 16:22:02 +0700

    Add files via upload

---
+++ Bai04.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{float chieuDai,chieuRong;
+printf("Nhap chieu dai:");
+scanf("%f",&chieuDai);
+printf("nhap chieu rong:");
+scanf("%f",&chieuRong);
+printf("Chu vi:%0.2f\n",(chieuDai+chieuRong)*2);
+printf("Dien thich:%0.2f",(chieuDai*chieuRong));
+}
