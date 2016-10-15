eaba22167bf526491d7acb6f25a67a8f8c570000
Trần Thị Ngọc <ngoctran1798@gmail.com>
2016-10-15 16:01:50 +0700

    Add files via upload

---
+++ Bai03.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    float a,chuVi,dienTich;
+    printf("Nhap canh hinh vuong: ");
+    scanf("%f",&a);
+    chuVi=a*4;
+    dienTich=a*a;
+    printf("Chu vi hinh vuong la: %10.2f\n",chuVi);
+    printf("Dien tich hinh vuong la: %10.2f",dienTich);
+}
+
